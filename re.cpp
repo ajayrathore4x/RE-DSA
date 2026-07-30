a7f87d5f80a3c6e36d33401dcf2f2e57d9906553
ajayrathore4x <ajayx0094@gmail.com>
2026-07-30 10:24:31 +0530

    print sub array of an array

--- re.cpp
+++ re.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+// for printing maximum subarray of a array-----||------
+void printSubArray(const vector<int>& arr){
+    for(int i=0;i<arr.size();i++){
+        for(int j=i;j<arr.size();j++){
+            for(int k=i;k<=j;k++){
+                cout<<arr[k];
+            }
+            cout<<endl;
+        }
+    }
+    return;
+}
 int main(){
-    cout<<"welcome to the dsa WORLD"<<endl;
+   vector<int>arr={1,2,3,4,5};
+   printSubArray(arr);
     return 0;
 }
