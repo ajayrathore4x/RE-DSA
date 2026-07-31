8a53315c892ccc4be164a644d7cb77575188bca2
ajayrathore4x <ajayx0094@gmail.com>
2026-07-31 08:48:01 +0530

    maximum sum of ans subarray

--- re.cpp
+++ re.cpp
@@ -14,6 +14,24 @@ void printSubArray(const vector<int>& arr){
     }
     return;
 }
+
+// for printing maximum sum of sub array-----||-----||----
+void printmaxsum(vector<int>& arr){
+    if(arr.empty()){
+        cout<<"0"<<endl;
+        return;
+    }
+    int maxsum=INT_MIN;
+    for(int i=0;i<arr.size();i++){
+       int cursum=0;
+        for(int j=i;j<arr.size();j++){
+            cursum+=arr[j];
+             maxsum=max(maxsum,cursum);
+        }
+    }
+    cout<<maxsum<<endl;
+    return;
+}
 int main(){
    vector<int>arr={1,2,3,4,5};
    printSubArray(arr);
