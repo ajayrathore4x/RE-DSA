f2759d48efbe49d3cf31d76eaad2337ad32b9ce0
ajayrathore4x <ajayx0094@gmail.com>
2026-08-11 07:48:26 +0530

    search single element in sorted array

--- re.cpp
+++ re.cpp
@@ -183,6 +183,39 @@ int peakIndexInMountainArray(vector<int>& arr) {
     } 
     return -1;
 }
+int singleNonDuplicate(vector<int>& nums) {
+    if(nums.empty()||nums.size()%2==0) return -1;
+    if(nums.size()==1) return nums[0];
+    int st=0;
+    int end=nums.size()-1;
+    while(st<=end){
+        int mid=st+((end-st)/2);
+        if((mid == 0 && nums[mid] != nums[mid+1]) ||
+        (mid == nums.size()-1 && nums[mid] != nums[mid-1]) ||
+        (mid > 0 && mid < nums.size()-1 &&
+            nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])){
+            return nums[mid];
+        }
+        if(mid%2==0){
+            if(nums[mid]==nums[mid-1]){
+                end=mid-1;
+            }
+            else{
+                st=mid+1;
+            }
+        }else{
+            if(nums[mid]==nums[mid+1]){
+                end=mid-1;
+            }
+            else{
+                st=mid+1;
+            }
+        }
+    }
+    return -1;
+    
+}
+
 int main(){
    vector<int>arr={1,2,2,1,1,2,2};
    cout<<majorityele1(arr)<<endl;
