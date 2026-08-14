ac099374105cd2f26bdbb215a32ee470f3d90427
ajayrathore4x <ajayx0094@gmail.com>
2026-08-14 10:39:04 +0530

    merge sorted array

--- re.cpp
+++ re.cpp
@@ -338,6 +338,27 @@ void sortColors(vector<int>& arr){
     }
     return ;
 }
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
+    int end=(m+n)-1;
+    int i=m-1;
+    int j=n-1;
+    while(i>=0&&j>=0){
+        if(nums1[i]<=nums2[j]){
+            nums1[end]=nums2[j];
+            end--;j--;
+        }
+        else{
+            nums1[end]=nums1[i];
+            end--;i--;
+        }
+    }
+    while(j>=0){
+        nums1[end]=nums2[j];
+        end--;j--;
+    }
+
+    return;
+}
 int main(){
    vector<int>arr={5,10,30,20,15};
    cout<<minTime(arr,3);
