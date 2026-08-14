55d73f7d215116f69dfafb24477a75122cad232a
ajayrathore4x <ajayx0094@gmail.com>
2026-08-14 10:19:49 +0530

    sort colors

--- re.cpp
+++ re.cpp
@@ -317,6 +317,27 @@ int aggressiveCows(int *arr, int n, int k) {
     }
     return end;
 }
+void sortColors(vector<int>& arr){
+    int low=0;
+    int mid=0;
+    int high=arr.size()-1;
+    while(mid<=high){
+        if(arr[mid]==0){
+            swap(arr[low],arr[mid]);
+            low++;
+            mid++;
+        }else if(arr[mid]==1){
+            mid++;
+        }else{
+            swap(arr[mid],arr[high]);
+            high--;
+        }
+    }
+    for(int val:arr){
+        cout<<val<<" ";
+    }
+    return ;
+}
 int main(){
    vector<int>arr={5,10,30,20,15};
    cout<<minTime(arr,3);
